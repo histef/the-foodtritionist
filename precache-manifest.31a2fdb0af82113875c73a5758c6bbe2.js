b405ad4ad571d5d40f482407dde286dc7f16af91
Stefanie Chin <hi.stef.c@gmail.com>
2018-12-27 03:06:57 -0800

    Updates

---
+++ precache-manifest.31a2fdb0af82113875c73a5758c6bbe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9672581bdfd522045ee7",
+    "url": "/the-foodtritionist/static/js/runtime~main.9672581b.js"
+  },
+  {
+    "revision": "76745c43e63794a73b57",
+    "url": "/the-foodtritionist/static/js/main.76745c43.chunk.js"
+  },
+  {
+    "revision": "74edcad572fb1242d88f",
+    "url": "/the-foodtritionist/static/js/1.74edcad5.chunk.js"
+  },
+  {
+    "revision": "76745c43e63794a73b57",
+    "url": "/the-foodtritionist/static/css/main.ecfbd3aa.chunk.css"
+  },
+  {
+    "revision": "74edcad572fb1242d88f",
+    "url": "/the-foodtritionist/static/css/1.97620603.chunk.css"
+  },
+  {
+    "revision": "0d7505ed9f2deac25c9e6fea99259b67",
+    "url": "/the-foodtritionist/index.html"
+  }
+];
